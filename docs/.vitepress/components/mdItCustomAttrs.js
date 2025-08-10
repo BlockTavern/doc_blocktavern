79ecb8ad405025ae247a532726550710a6ca1d03
Re0XIAOPA <mckekejun@outlook.com>
2025-08-10 15:31:54 +0800

    背景色

--- docs/.vitepress/components/mdItCustomAttrs.js
+++ docs/.vitepress/components/mdItCustomAttrs.js
@@ -1,3 +1,11 @@
+/**
+ * 自定义 markdown 插件 - 图片预览
+ * 可以查看使用 https://www.awafuns.cn/tooldoc/zh/tutorial/Vitepress/Vitepress%E5%9B%BE%E7%89%87%E7%BC%A9%E6%94%BE%E9%A2%84%E8%A7%88.html
+ * 1. 图片添加 fancybox 属性
+ * 2. ![图片描述](图片地址)
+ * 3. <img src="图片地址" data-fancybox="gallery"/>
+ */
+
 import mdItCustomAttrs from "markdown-it-custom-attrs";
 
 export const mdItCustomAttrsConfig = {
